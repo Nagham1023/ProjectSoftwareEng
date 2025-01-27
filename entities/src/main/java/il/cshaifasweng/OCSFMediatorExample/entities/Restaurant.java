a76aa48ae9b10b088a9a5b20cf8ee4f2286ae1cd
Adan Bokaee <adan652003@gmail.com>
2025-01-27 15:50:42 +0200

    with mainScreen and Choose Branch

---
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/Restaurant.java
@@ -0,0 +1,88 @@
+package il.cshaifasweng.OCSFMediatorExample.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "restaurants")
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "restaurant_name", nullable = false)
+    private String restaurantName;
+
+    @Column(name = "image_path")
+    private String imagePath;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Meal> meals;
+
+    // Default constructor
+    public Restaurant() {
+    }
+
+    // Constructor with parameters
+    public Restaurant(int id, String restaurantName, String imagePath, String phoneNumber) {
+        this.id = id;
+        this.restaurantName = restaurantName;
+        this.imagePath = imagePath;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Meal> meals) {
+        this.meals = meals;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant" +
+                "ID='" + getId() + '\'' +
+                ", RestaurantName='" + getRestaurantName() + '\'' +
+                ", IMG='" + getImagePath() + '\'' +
+                ", PhoneNumber='" + getPhoneNumber() + '\'' +
+                '}';
+    }
+}
